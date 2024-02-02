f46e7a5d2b307f76caf021ede13ae3b7f5b96c83
Santeri Paajanen <paajanensanteri@gmail.com>
2024-02-02 18:21:19 +0200

    Added lockfiling

--- backup_to_allas.sh
+++ backup_to_allas.sh
@@ -11,8 +11,47 @@ function tee_with_timestamps () {
 }
 
 
+function aquire_lock () {
+    local lockfile=$1
+    local logfile=$2
+    iter=0
+    maxiter=60
+    sleeptime=60
+    while -f $lockfile; do
+        read pid < cat $lockfile
+        if ps -p $pid > /dev/null; then
+            if [[ iter -gt maxiter ]]; then
+                echo "Failed to aquire lock after $iter minutes" | tee_with_timestamps $logfile
+                return 1
+            fi 
+            echo "$pid has lockfile, sleeping 60 s..." | tee_with_timestamps $logfile
+            sleep 60
+        else
+            echo $lockfile is stale, removing!!!!! | tee_with_timestamps $logfile
+            rm $lockfile
+        fi
+        let iter++
+    done
+
+    echo $$ > $lockfile
+    echo "Aquired lockfile" $lockfile | tee_with_timestamps $logfile
+    return 0
+}
+
+
+logfile=~/.allas_bu_confs/logfile.log
+lockfile=~/.allas_bu_confs/lockfile.lock
+
+
+if ! aquire_lock $lockfile $logfile ; then
+    echo Cannot continue without lock file, please try again later. | tee_with_timestamps $logfile
+    exit 1
+fi
+
 while read -r line || [[ -n "$line" ]]; do
     arr=($line)
-    echo Running s3cmd -c ~/.allas_bu_confs/project_${arr[0]} --no-progress sync ${arr[1]} s3://${arr[2]} 2>&1 | tee_with_timestamps ~/.allas_bu_confs/backups_${arr[0]}.log
-    s3cmd -c ~/.allas_bu_confs/project_${arr[0]} --no-progress sync ${arr[1]} s3://${arr[2]} 2>&1 | tee_with_timestamps ~/.allas_bu_confs/backups_${arr[0]}.log
+    echo Running s3cmd -c ~/.allas_bu_confs/project_${arr[0]} --no-progress sync ${arr[1]} s3://${arr[2]} | tee_with_timestamps $logfile
+    s3cmd -c ~/.allas_bu_confs/project_${arr[0]} --no-progress sync ${arr[1]} s3://${arr[2]} 2>&1 | tee_with_timestamps $logfile
 done < ~/.allas_bu_confs/config
+
+rm $lockfile
